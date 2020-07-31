e67f7954d1cd056e58cefa15d4f898d76c90c525
rubens-lavor <haryson07@gmail.com>
2020-07-31 14:05:00 -0300

    estilizando o formulário

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -2,8 +2,68 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Input = styled.input`
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type="color"] {
+    padding-left: 56px;
+  }
 
+`;
+
+const Label = styled.label`
+`;
+
+Label.Text = styled.span`
+color: #E5E5E5;
+height: 57px;
+position: absolute; 
+top: 0;
+left: 16px;
+
+display: flex;
+align-items: center;
+
+transform-origin: 0% 0%;
+font-size: 18px;
+font-style: normal;
+font-weight: 300;
+
+transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+  
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+  
+  padding: 16px 16px;
+  margin-bottom: 45px;
+  
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+  
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  &:focus:not([type='color']) + ${Label.Text} {
+    transform: scale(.6) translateY(-10px);
+  }
+
+  &:focus:not([type="color"]) + span {
+    background:red;
+  }
 `;
 
 function FormField({
@@ -13,13 +73,12 @@ function FormField({
   // eslint-disable-next-line react/destructuring-assignment
   const fielId = `id_${name}`;
   const isTextarea = type === "textarea"
-  const Tag = isTextarea ? "textarea":"input";
+  const Tag = isTextarea ? "textarea" : "input";
   return (
-    <div>
-      <label
+    <FormFieldWrapper>
+      <Label
         htmlFor={fielId}
       >
-        {label}
         <Input
           as={Tag}
           id={fielId}
@@ -28,15 +87,20 @@ function FormField({
           value={value}
           onChange={onChange}
         />
-      </label>
-    </div>
+
+        <Label.Text>
+          {label}
+        </Label.Text>
+
+      </Label>
+    </FormFieldWrapper>
   );
 }
 
 FormField.defaultProps = {
   type: 'text',
   value: '',
-  onChange: () => {},
+  onChange: () => { },
 };
 
 FormField.prototype = {
